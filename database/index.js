83d07d9b013b8bab8a209affbd4845c5e07c0c7b
NickRashad <nrsternes@gmail.com>
2019-08-22 23:14:18 -0500

    Connect Postgres & Add Table

--- database/index.js
+++ database/index.js
@@ -1,50 +1,39 @@
-const mongoose = require('mongoose');
+/* eslint-disable no-console */
+// const mongoose = require('mongoose');
+const { Client } = require('pg');
 
-const url = 'mongodb://localhost';// localhost';
-const mongoDBURI = process.env.MONGOLAB_URI || url;
-mongoose.connect(mongoDBURI,
-  {
-    dbName: 'Yelp',
-    useNewUrlParser: true,
-  }).then(() => console.log('mongoDB connected'))
-  .catch(err => console.log(err));
-
-const db = mongoose.connection;
-
-const bizSchema = mongoose.Schema({
-  bId: Number,
-  bizname: String,
-  reviewCount: Number,
-  rating: Number,
-  price: String,
-  category: [String],
-  location: Object,
-  phone: String,
-  url: String,
-  photos: [Number],
+const client = new Client({
+  database: 'bizschema',
 });
 
-const Biz = mongoose.model('Biz', bizSchema);
+// const connect = async () => {
+//   await client.connect();
+//   // const res = await client.query('CREATE DATABASE bizSchema');
+//   // console.log(res);
+//   const res = await client.query('SELECT $1::text as stat', ['Database successfully connected']);
+//   console.log(res.rows[0].stat);
+//   await client.end();
+// };
 
-const photoSchema = mongoose.Schema({
-  pId: Number,
-  imgUrl: String,
-  uId: Number,
-  bId: Number,
-  text: String,
-  tag: String,
-});
-const Photo = mongoose.model('Photo', photoSchema);
-
-const userSchema = mongoose.Schema({
-  uId: Number,
-  userav: String,
-  username: String,
-});
-const User = mongoose.model('User', userSchema);
-module.exports = {
-  Biz,
-  Photo,
-  User,
-  db,
+const createTable = async () => {
+  await client.connect();
+  const res = await client.query(`CREATE TABLE IF NOT EXISTS biz(
+    bId       integer primary key,
+    bizname   text,
+    reviewCount integer,
+    rating    decimal,
+    price     decimal,
+    category  text[],
+    location  text,
+    phone     char(14),
+    url       text,
+    photos    integer[]
+  );`);
+  console.log(res);
+  await client.end();
 };
+
+// connect();
+createTable();
+
+module.exports = { client };
